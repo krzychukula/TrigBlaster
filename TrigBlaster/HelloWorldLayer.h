1e84d1b4b17bdc2c686cc2f27cd4300ca2f1f713
Krzysztof Kula <krzychukula@gmail.com>
2014-07-26 15:57:48 +0200

    make it simpler and purple

--- TrigBlaster/HelloWorldLayer.h
+++ TrigBlaster/HelloWorldLayer.h
@@ -1,23 +1,7 @@
-//
-//  HelloWorldLayer.h
-//  TrigBlaster
-//
-//  Created by Krzysztof Kula on 26.07.2014.
-//  Copyright Tutorial 2014. All rights reserved.
-//
-
-
-#import <GameKit/GameKit.h>
-
-// When you import this file, you import all the cocos2d classes
 #import "cocos2d.h"
 
-// HelloWorldLayer
-@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
-{
-}
+@interface HelloWorldLayer : CCLayerColor
 
-// returns a CCScene that contains the HelloWorldLayer as the only child
-+(CCScene *) scene;
++ (CCScene *)scene;
 
 @end
