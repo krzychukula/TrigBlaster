5fb082bcfaedb943e258940a0322132aba5a92f2
Krzysztof Kula <krzychukula@gmail.com>
2014-07-26 15:53:23 +0200

    tutorial

---
+++ TrigBlaster/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  TrigBlaster
+//
+//  Created by Krzysztof Kula on 26.07.2014.
+//  Copyright Tutorial 2014. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ TrigBlaster/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TrigBlaster
+//
+//  Created by Krzysztof Kula on 26.07.2014.
+//  Copyright Tutorial 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+        return retVal;
+    }
+}
